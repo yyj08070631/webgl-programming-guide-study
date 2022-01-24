26f3c888009547358c66a369d895c6e6fc54897d
yangyujian <yangyujian@corp.netease.com>
2022-01-24 20:06:11 +0800

    update MultiPoints

---
+++ ch03/MultiPoints/main.js
@@ -0,0 +1,83 @@
+let VSHADER_SOURCE = null
+let FSHADER_SOURCE = null
+
+function main () {
+    const canvas = document.getElementById('webgl')
+    const gl = getWebGLContext(canvas, true)
+    loadShaderFile(gl, 'vert.vert', gl.VERTEX_SHADER)
+    loadShaderFile(gl, 'frag.frag', gl.FRAGMENT_SHADER)
+
+    function start () {
+        if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+            console.log('failed to initialize shaders.')
+            return
+        }
+        const a_Position = gl.getAttribLocation(gl.program, 'a_Position')
+        const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor')
+        // canvas.onmouseup = function (e) { click(e, gl, canvas, a_Position, u_FragColor, n) }
+        gl.clearColor(0.0, 0.0, 0.0, 1.0)
+        gl.clear(gl.COLOR_BUFFER_BIT)
+        const n = initVertexBuffers(gl, a_Position, u_FragColor)
+        if (n < 0) {
+            console.log('Failed to set the positions of the vertices')
+            return
+        }
+        gl.drawArrays(gl.POINTS, 0, n)
+    }
+
+    // const g_points = []
+    // function click (e, gl, canvas, a_Position, u_FragColor, n) {
+    //     let x = e.clientX
+    //     let y = e.clientY
+    //     const c = [Math.random(), Math.random(), Math.random(), 1.0]
+    //     const rect = e.target.getBoundingClientRect()
+
+    //     x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2)
+    //     y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2)
+    //     g_points.push({ x, y, c })
+    //     gl.clear(gl.COLOR_BUFFER_BIT)
+    //     for (let i = 0, len = g_points.length; i < len; i++) {
+    //         gl.vertexAttrib3f(a_Position, g_points[i].x, g_points[i].y, 0.0)
+    //         gl.uniform4f(u_FragColor, ...g_points[i].c)
+    //         gl.drawArrays(gl.POINTS, 0, n)
+    //     }
+    // }
+
+    function initVertexBuffers (gl, a_Position, u_FragColor) {
+        const vertices = new Float32Array([0.0, 0.5, 0.0, 0.0, -0.5, 0.0, 0.5, 0.0, 0.0])
+        const n = 3
+
+        const vertexBuffer = gl.createBuffer()
+        if (!vertexBuffer) {
+            console.log('Failed to create the buffer object')
+            return -1
+        }
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer)
+        gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+        gl.vertexAttribPointer(a_Position, n, gl.FLOAT, false, 0, 0)
+        gl.uniform4f(u_FragColor, 1.0, 0.0, 0.0, 1.0)
+        gl.enableVertexAttribArray(a_Position)
+        return n
+    }
+
+    function loadShaderFile (gl, fileName, shader) {
+        const request = new XMLHttpRequest()
+        request.onreadystatechange = function () {
+            if (request.readyState === 4 && request.status !== 404) {
+                onLoadShader(gl, request.responseText, shader)
+            }
+        }
+        request.open('GET', fileName, true)
+        request.send()
+    }
+    
+    function onLoadShader (gl, fileString, type) {
+        if (type === gl.VERTEX_SHADER) {
+            VSHADER_SOURCE = fileString
+        } else if (type === gl.FRAGMENT_SHADER) {
+            FSHADER_SOURCE = fileString
+        }
+        if (VSHADER_SOURCE && FSHADER_SOURCE) start(gl)
+    }
+}
