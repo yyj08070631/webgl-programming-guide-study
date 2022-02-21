dcd4527aa482c862bf74386421e83cdebe0ed33c
yangyujian <yangyujian@corp.netease.com>
2022-02-21 15:11:08 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# 任一步骤执行失败都会终止整个部署过程
+set -e
+
+printf "\033[0;32mCommitting updates to Git...\033[0m\n"
+
+# 另存本地修改
+git stash -u
+
+# 拉取最新代码
+git pull
+
+# 取出本地修改
+git stash pop
+
+# 添加更改到 git
+git add --all
+
+# 提交更改
+git commit -m "$1"
+
+# 推送到远程仓库
+git push --force origin main
