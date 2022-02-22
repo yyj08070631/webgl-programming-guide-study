949c11d36e7764916936032e214982f36785e4ce
yangyujian <yangyujian@corp.netease.com>
2022-02-22 15:51:08 +0800

    Update by sync.sh

--- ch06/Zfighting/main.js
+++ ch06/Zfighting/main.js
@@ -29,7 +29,7 @@ function main () {
         const viewMatrix = new Matrix4()
         const projMatrix = new Matrix4().setPerspective(30, canvas.width / canvas.clientHeight, 1, 100)
         const modelMatrix = new Matrix4()
-        let g_eyeX = 0, g_eyeY = 0, g_eyeZ = 10
+        let g_eyeX = 0, g_eyeY = 0, g_eyeZ = 15, currDeg = 0
 
         document.onkeydown = function (e) { keydown(e, gl, n, u_ViewMatrix, viewMatrix, u_ModelMatrix, modelMatrix) }
 
@@ -37,11 +37,13 @@ function main () {
         draw(gl, n, u_ViewMatrix, viewMatrix, u_ModelMatrix, modelMatrix)
         
         function keydown (e, gl, n, u_ViewMatrix, viewMatrix, u_ModelMatrix, modelMatrix) {
+            const updateX = deg => Math.sin(deg * Math.PI / 180) * 15
+            const updateZ = deg => Math.cos(deg * Math.PI / 180) * 15
             const map = {
-                39: () => { g_eyeX += 0.1 }, // 方向键右
-                37: () => { g_eyeX -= 0.1 }, // 方向键左
-                38: () => { g_eyeY += 0.1 }, // 方向键上
-                40: () => { g_eyeY -= 0.1 }, // 方向键下
+                39: () => { currDeg += 1; g_eyeX = updateX(currDeg); g_eyeZ = updateZ(currDeg) }, // 方向键右
+                37: () => { currDeg -= 1; g_eyeX = updateX(currDeg); g_eyeZ = updateZ(currDeg) }, // 方向键左
+                // 38: () => { g_eyeY += 0.1 }, // 方向键上
+                // 40: () => { g_eyeY -= 0.1 }, // 方向键下
             }
             const fn = map[e.keyCode] || (() => { console.log(`Key ${e.keyCode} not found.`) })
             fn()
@@ -51,20 +53,20 @@ function main () {
         function draw (gl, n, u_ViewMatrix, viewMatrix, u_ModelMatrix, modelMatrix) {
             viewMatrix.setLookAt(
                 g_eyeX, g_eyeY, g_eyeZ, // 视点
-                0, 0, -2, // 观察目标点
+                0, 0, -5, // 观察目标点
                 0, 1, 0, // 上方向
             )
             gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements)
             modelMatrix.setTranslate(0.75, 0, 0)
             gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements)
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-            gl.disable(gl.POLYGON_OFFSET_FILL)
+            // gl.disable(gl.POLYGON_OFFSET_FILL)
             gl.drawArrays(gl.TRIANGLES, 0, n / 2)
 
             modelMatrix.setTranslate(-0.75, 0, 0)
             gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements)
-            gl.enable(gl.POLYGON_OFFSET_FILL)
-            gl.polygonOffset(1.0, 1.0)
+            // gl.enable(gl.POLYGON_OFFSET_FILL)
+            // gl.polygonOffset(1.0, 1.0)
             gl.drawArrays(gl.TRIANGLES, n / 2, n / 2)
         }
     }
@@ -76,9 +78,9 @@ function main () {
             -2.5, -2.5,  -5.0,  0.4,  1.0,  0.4,
              2.5, -2.5,  -5.0,  1.0,  0.4,  0.4, 
         
-             0.0,  3.0,  -5.0,  1.0,  0.4,  0.4, // The yellow triagle
-            -3.0, -3.0,  -5.0,  1.0,  1.0,  0.4,
-             3.0, -3.0,  -5.0,  1.0,  1.0,  0.4, 
+             0.0,  3.0,  -3.0,  1.0,  0.4,  0.4, // The yellow triagle
+            -3.0, -3.0,  -3.0,  1.0,  1.0,  0.4,
+             3.0, -3.0,  -3.0,  1.0,  1.0,  0.4, 
         ])
         const FSIZE = verticesAndSizes.BYTES_PER_ELEMENT
         const n = 6
